f8c47d47f3f9db2275e0c80f57a050e94f30487a
Sun Gyu Park <spark2@macalester.edu>
2022-01-23 01:16:20 +0900

    Working on OMP sampling

--- data_collect/main.c
+++ data_collect/main.c
@@ -3,14 +3,12 @@
 # include <omp.h>
 # include <math.h>
 
-void seedThreads(unsigned int* seeds); //Generate unique seeds for each thread.
-void computeSave(FILE* res);
+void seedThreads(unsigned int* seedsX, unsigned int* seedsY); //Generate unique seeds for each thread.
+void computeSave(unsigned int* seedsX, unsigned int* seedsY);
 
 int main()
 {
-	/*FILE *res;
-
-	res = fopen("Data1.csv", "w+");*/
+	
 
 	omp_set_num_threads(4);
 
@@ -18,9 +16,10 @@ int main()
 
 	//int id;
 
-	unsigned int* seeds = (unsigned int*)malloc(sizeof(int)*nThreads);
+	unsigned int* seedsX = (unsigned int*)malloc(sizeof(int)*nThreads);
+	unsigned int* seedsY = (unsigned int*)malloc(sizeof(int)*nThreads);
 
-	seedThreads(seeds);
+	seedThreads(seedsX, seedsY);
 	
 	//printf("The first seed is %d", seeds[0]);
 
@@ -36,7 +35,7 @@ int main()
 	return 0;
 }
 
-void seedThreads(unsigned int* seeds) {
+void seedThreads(unsigned int* seedsX, unsigned int* seedsY) {
 
 #	pragma omp parallel
 	{
@@ -44,8 +43,43 @@ void seedThreads(unsigned int* seeds) {
 
 		unsigned int seed = (unsigned)time(NULL);
 
-		seeds[threadId] = (seed & 0xFFFFFFF0) | (threadId + 1);
+		seedsX[threadId] = (seed & 0xFFFFFFF0) | (threadId + 1);
+		seedsY[threadId] = (seed & 0xFFF0F0F0) | (threadId + 1);
 		// printf("thread %d has seed %d \n", threadId, seed);
 	}
 
+}
+
+void computeSave(unsigned int* seedsX, unsigned int* seedsY)
+{
+	FILE *res;
+	res = fopen("Data1.csv", "w+");
+
+	int i;
+	unsigned int seedX, seedY;
+	float x, y, d;
+
+# pragma omp parallel \
+  shared(res) private(i,seedX, seedY,x,y,d)
+  {
+    seedX=seedsX[omp_get_thread_num()];
+	seedY=seedsY[omp_get_thread_num()];
+    
+	#pragma omp for schedule(static)
+    for(i = 0; i < 100000; i++){
+	  srand(seedX);
+      x = (float) (rand_r(&seedX) % 300);
+	  srand(seedY);
+	  y = (float) (rand_r(&seedY) % 300);
+	  d = sqrt(pow(x,2)+pow(y,2));
+	
+	# pragma omp critical
+	  fprintf(res, "%.4f, %.4f, %.4f \n", x,y,d);
+      
+      
+      // printf("Thread %d: %c for seed %d\n", omp_get_thread_num(), res[i], i);
+      
+    }
+  }
+  fclose(res);
 }
